c8a89252360152f748efed0c09f7049a8c3682f8
Imad Aqil <imaqil.mj@gmail.com>
2025-04-21 12:35:23 +0700

    pasword

--- admin/login.php
+++ admin/login.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if ($username === 'admin' && $password === 'admin123') {
+    if ($username === 'admin' && $password === 'diGzkivQ#ads984823m') {
         $_SESSION['logged_in'] = true;
         header('Location: index.php');
         exit;
